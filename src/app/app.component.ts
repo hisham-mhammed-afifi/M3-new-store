a4283d9bc9d6a65ca9b369ca3fa2aedef1d649df
hesham-afifi <hesham.mohamed@widebot.net>
2024-04-22 10:53:13 +0200

    add button to toggle themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, signal } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSidenavModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'new-store';
+
+  isDark = signal(this.document.body.classList.contains('dark'));
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  toggleTheme() {
+    if (this.isDark()) {
+      this.document.body.classList.remove('dark');
+      this.isDark.set(false);
+    } else {
+      this.document.body.classList.add('dark');
+      this.isDark.set(true);
+    }
+  }
 }
